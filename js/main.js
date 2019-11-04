645b4ac2700f2277e0a37713b5d7f776cbf79416
Mephistofeel <nogib228@gmail.com>
2019-11-04 13:44:27 +0300

    Стилизовал 3 секцию и оживил слайдер

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+
+  $(".reviews-slider").slick({
+    infinite: true,
+    autoplay: true,
+    autoplaySpeed: 7500,
+    slidesToShow: 2,
+    slidesToScroll: 2,
+    dots: true,
+    arrows: false
+  });
+
+});
